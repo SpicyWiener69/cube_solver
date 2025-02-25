6c333742b57cf08a43339187a30bba61bc422020
aaron <dyhong2002@gmail.com>
2025-02-25 21:07:18 +0800

    added new function for effector abs position

--- python_CV_Parser/notation_movement_v2.py
+++ python_CV_Parser/notation_movement_v2.py
@@ -1,42 +1,39 @@
 from icecream import ic
 from dataclasses import dataclass
 
+@dataclass
+class Dim3x3:
 
-class Dimension3x3:
+    SIDE_LEN = 50
+    SLICE_LEN = 50/3
 
+    #EFFECTOR D (DOWN)
+    D_HOME = 30
+    D_LAYER_TOP =D_HOME +SLICE_LEN * 1
+    D_LAYER_MID =D_HOME +SLICE_LEN * 2        
+    D_LAYER_ALL =D_HOME +SLICE_LEN * 3
 
-    side_len = 50
-    slice_len = 50/3
-
-    #effector d (down)
-    d_home = 30
-    d_layer_top =d_home +slice_len * 1
-    d_layer_mid =d_home +slice_len * 2        
-    d_layer_all =d_home +slice_len * 3
-
-    #effector c(clamping)
-    c_home_offset = 5
-    c_home =side_len + c_home_offset
-    c_clamp =side_len
+    #EFFECTOR C(CLAMPING)
+    C_HOME_OFFSET = 5
+    C_HOME =SIDE_LEN + C_HOME_OFFSET
+    C_CLAMP =SIDE_LEN
     
-    #effector g (gripper)
-    g_home =side_len
-    g_offset = 5
-    g_release =side_len + g_offset
+    #EFFECTOR G (GRIPPER)
+    G_OFFSET = 5
+    G_HOME = SIDE_LEN + G_OFFSET
+    G_GRIP = SIDE_LEN
 
 @dataclass
 class NotationDataClass:
     #default values
     name:str = "default"
     direction:int = 1
-    repetition:int =1
+    repetition:int = 1
 
     #printing:
     def __str__(self):
         return f"{self.name}{self.direction}{self.repetition})"
     
-
-
 def notations_to_dataclasses(notations): #->list[NotationDataClass]
     dataclasses = []
     for notation in notations:
@@ -57,13 +54,6 @@ def notations_to_dataclasses(notations): #->list[NotationDataClass]
 
 
     return dataclasses
-
-
-
-def dataclasses_to_effector_movement(dataclasses): #->list[NotationDataClass]
-
-    pass
-
 
 def notations_to_modified_notations(dataclasses):
     modified_dataclasses = []
@@ -130,8 +120,73 @@ def remove_repetitions(modified_dataclasses):    #->list[NotationDataClass]
     #     if i >= len(modified_dataclasses - 2): 
     #         cleaned_dataclasses.append(data2)
 
+
+@dataclass
+class EffectorMovement:
+    Effector:str
+    movement:int
+    isrelative:bool = False
     
-    return cleaned_dataclasses
+
+def dataclasses_to_effector_abs(dataclasses):
+    abs_states = []
+
+    notation_to_commands = {
+        'x':lambda dir,rep: [
+                            EffectorMovement(Effector = 'D', movement = Dim3x3.D_HOME),
+                            EffectorMovement(Effector = 'C', movement = Dim3x3.C_CLAMP),
+                            EffectorMovement(Effector = 'LR', movement = (90 * dir * rep), isrelative=True),
+                            EffectorMovement(Effector = 'C', movement =Dim3x3.C_HOME)
+                            ],
+
+        'y':lambda dir,rep: [
+                            EffectorMovement(Effector = 'D', movement = Dim3x3.D_LAYER_TOP),
+                            EffectorMovement(Effector = 'G', movement = Dim3x3.G_GRIP),
+                            EffectorMovement(Effector = 'T', movement = 90 * dir, isrelative=True),
+                            EffectorMovement(Effector = 'D', movement = Dim3x3.D_HOME),
+                            EffectorMovement(Effector = 'T', movement = (90 * dir * rep * -1), isrelative=True),
+                            EffectorMovement(Effector = 'G', movement = Dim3x3.G_HOME),
+                            ],   
+                               
+        'U':lambda dir,rep: [
+                            EffectorMovement(Effector = 'D', movement = Dim3x3.D_LAYER_TOP),
+                            EffectorMovement(Effector = 'C', movement = Dim3x3.C_CLAMP),
+                            EffectorMovement(Effector = 'LR', movement = 90 * dir * rep, isrelative=True),
+                            EffectorMovement(Effector = 'C', movement =Dim3x3.C_HOME)
+                            ],
+        'u':lambda dir,rep: [
+                            EffectorMovement(Effector = 'D', movement = Dim3x3.D_LAYER_MID),
+                            EffectorMovement(Effector = 'C', movement = Dim3x3.C_CLAMP),
+                            EffectorMovement(Effector = 'LR', movement = 90 * dir * rep, isrelative=True),
+                            EffectorMovement(Effector = 'C', movement =Dim3x3.C_HOME)
+                            ],
+    }
+
+
+    for notation in dataclasses:
+        movement = notation_to_commands.get(notation.name)(notation.direction, notation.repetition)
+        abs_states.extend(movement)
+    return abs_states
+
+#note: only linear effectors need abs->relative conversions 
+# def effector_abs_to_relative(commands): #->list[NotationDataClass]
+    
+#     @dataclass
+#     class AbsEffectorState:
+#         #default position
+#         # angles in degrees, distance in mm:
+#         D_dis:int = 30
+#         C_dis:int = 50
+#         G_angle:int = 66
+#         R_angle:int = 90
+
+
+
+#     abs_effector_state = AbsEffectorState()
+
+#     for command in commands:
+
+
 
 if __name__ == "__main__":
     #notations = ['U',"U'","U'","U","R","R'","D"]
@@ -141,5 +196,7 @@ if __name__ == "__main__":
     modified_dataclasses = notations_to_modified_notations(dataclasses)
     ic(modified_dataclasses)
     cleaned_dataclasses = remove_repetitions(modified_dataclasses)
-    ic(cleaned_dataclasses)
+    ic(cleaned_dataclasses,len(cleaned_dataclasses))
+    effector_abs = dataclasses_to_effector_abs(cleaned_dataclasses)
+    ic(effector_abs,len(effector_abs))
 
